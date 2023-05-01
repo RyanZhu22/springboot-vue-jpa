ad4ad87dc816b733f40c5b4c0b07fc34c3d61472
ryanzhu <ryanzhu.work@gmail.com>
2023-05-01 20:31:54 +0800

    更新: aside icon图标

--- vue/src/views/Aside.vue
+++ vue/src/views/Aside.vue
@@ -1,88 +1,105 @@
 <template>
-    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse" @open="handleOpen"
-        @close="handleClose">
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    :collapse="isCollapse"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <el-radio-group
+      v-model="isCollapse"
+      style="margin-bottom: 20px; align-items: flex-start"
+    >
+      <el-radio-button :label="false" size="small">expand</el-radio-button>
+      <el-radio-button :label="true" size="small">collapse</el-radio-button>
+    </el-radio-group>
 
-        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px; align-items: flex-start;">
-            <el-radio-button :label="false" size="small">expand</el-radio-button>
-            <el-radio-button :label="true"  size="small">collapse</el-radio-button>
-        </el-radio-group>
-
-        <el-menu-item index="1" @click="toHome">
-            <el-icon><location /></el-icon>
-            <template #title >
-                Home
-            </template>
-        </el-menu-item>
-        <el-menu-item index="2" @click="toSystem">
-            <el-icon><icon-menu /></el-icon>
-            <template #title>System Management</template>
-        </el-menu-item>
-        <el-menu-item index="3" @click="toUser">
-            <el-icon><document /></el-icon>
-            <template #title to="/user">User Management</template>
-        </el-menu-item>
-        <el-menu-item index="4" @click="toRole">
-            <el-icon><setting /></el-icon>
-            <template #title to="/role">Role Management</template>
-        </el-menu-item>
-        <el-menu-item index="5" @click="toPermission">
-            <el-icon><setting /></el-icon>
-            <template #title to="/permission">Permission Management</template>
-        </el-menu-item>
-        <el-menu-item index="6" @click="toFile">
-            <el-icon><setting /></el-icon>
-            <template #title to="/files">File Management</template>
-        </el-menu-item>
-    </el-menu>
+    <el-menu-item index="1" @click="toHome">
+      <el-icon>
+        <location />
+      </el-icon>
+      <template #title> Home </template>
+    </el-menu-item>
+    <el-menu-item index="2" @click="toSystem">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>System Management</template>
+    </el-menu-item>
+    <el-menu-item index="3" @click="toUser">
+      <el-icon>
+        <UserFilled />
+      </el-icon>
+      <template #title to="/user">User Management</template>
+    </el-menu-item>
+    <el-menu-item index="4" @click="toRole">
+      <el-icon>
+        <document />
+      </el-icon>
+      <template #title to="/role">Role Management</template>
+    </el-menu-item>
+    <el-menu-item index="5" @click="toPermission">
+      <el-icon>
+        <Flag />
+      </el-icon>
+      <template #title to="/permission">Permission Management</template>
+    </el-menu-item>
+    <el-menu-item index="6" @click="toFile">
+      <el-icon>
+        <List />
+      </el-icon>
+      <template #title to="/files">File Management</template>
+    </el-menu-item>
+  </el-menu>
 </template>
-  
-<script setup>
-import router from '../router'
-import { ref } from 'vue'
-import {
-    Document,
-    Menu as IconMenu,
-    Location,
-    Setting,
-} from '@element-plus/icons-vue'
 
-const isCollapse = ref(true)
+<script setup>
+import router from "../router";
+import { ref } from "vue";
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+  UserFilled,
+  List,
+  Flag,
+} from "@element-plus/icons-vue";
+
+const isCollapse = ref(true);
 const handleOpen = (key, keyPath) => {
-    console.log(key, keyPath)
-}
+  console.log(key, keyPath);
+};
 const handleClose = (key, keyPath) => {
-    console.log(key, keyPath)
-}
+  console.log(key, keyPath);
+};
 
 const toHome = () => {
-    router.push('/home')
-}
+  router.push("/home");
+};
 
 const toSystem = () => {
-    router.push('/system')
-}
+  router.push("/system");
+};
 
 const toUser = () => {
-    router.push('/user')
-}
+  router.push("/user");
+};
 
 const toRole = () => {
-    router.push('/role')
-}
+  router.push("/role");
+};
 
 const toPermission = () => {
-    router.push('/permission')
-}
+  router.push("/permission");
+};
 
 const toFile = () => {
-    router.push('/file')
-}
+  router.push("/file");
+};
 </script>
-  
+
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
+  width: 200px;
+  min-height: 400px;
 }
 </style>
-  
